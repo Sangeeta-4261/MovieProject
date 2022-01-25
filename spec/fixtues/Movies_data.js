615d6d4dc715d59ab675fce70f10056a2fcaf819
Sangeeta <81579025+Sangeeta-4261@users.noreply.github.com>
2022-01-25 17:29:49 +0530

    Add files via upload

---
+++ spec/fixtues/Movies_data.js
@@ -0,0 +1,32 @@
+
+
+//restorents information are stored in this file using Fixtures
+var FIXTURES = window.FIXTURES || {};
+
+FIXTURES.movies ={
+
+    movie1:{
+        name:"Sarathi",
+        rating:"4.5",
+        genres:"comedy",
+        comment:"very Good"
+    },
+    movie2:{
+        name:"Rajkumar",
+        rating:"4.8",
+        genres:"adventure",
+        comment:"excelent movie"
+    },
+    movie3:{
+        name:"Milana",
+        rating:"4.3",
+        genres:"action",
+        comment:"Good"
+    },
+    movie4:{
+        name:"Robert",
+        rating:"4.5",
+        genres:"horror",
+        comment:"Good"
+    },
+}
